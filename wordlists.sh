bc65aca29b003f1ac07dfbe6f9ef2965fb44ce89
Crashtastic314159 <97134474+Crashtastic314159@users.noreply.github.com>
2022-01-05 10:31:00 +1100

    Create wordlists.sh

---
+++ wordlists.sh
@@ -0,0 +1,6 @@
+#!/bin.bash
+
+sudo apt update && sudp apt install wordlists
+sudo mkdir /usr/share/wordlists/SecLists
+sudo chown $USER /usr/share/wordlists/SecLists
+git clone https://github.com/danielmiessler/SecLists /usr/share/wordlists
