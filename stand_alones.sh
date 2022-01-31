c6b16df115ff0fdba80f6f31325a9ed53eca4a6c
Crashtastic314159 <97134474+Crashtastic314159@users.noreply.github.com>
2022-02-01 10:59:52 +1100

    Update stand_alones.sh

--- stand_alones.sh
+++ stand_alones.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install chisel
-sudo apt install ffuf
-sudo apt install python3-venv
-sudo gem install evil-winrm
+sudo apt install chisel -y
+sudo apt install ffuf -y
+sudo apt install python3-venv -y 
+sudo gem install evil-winrm -y
