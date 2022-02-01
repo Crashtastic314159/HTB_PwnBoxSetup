eb1463eb85aa317c418e1439bb06885cf5804033
Crashtastic314159 <97134474+Crashtastic314159@users.noreply.github.com>
2022-02-01 11:00:07 +1100

    Update wordlists.sh

--- wordlists.sh
+++ wordlists.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install wordlists
+sudo apt update && sudo apt install wordlists -y
 sudo mkdir /usr/share/wordlists/SecLists
 sudo chown $USER /usr/share/wordlists/SecLists
 git clone https://github.com/danielmiessler/SecLists /usr/share/wordlists/SecLists
