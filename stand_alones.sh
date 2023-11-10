a3c52cc72228758578c31b838d3d2d8a91a34faa
Crashtastic314159 <97134474+Crashtastic314159@users.noreply.github.com>
2023-11-10 15:25:47 +1100

    Update stand_alones.sh

--- stand_alones.sh
+++ stand_alones.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sudo apt install chisel -y
 sudo apt install ffuf -y
 sudo apt install python3-venv -y 
 sudo apt install radare2 -y
 sudo gem install evil-winrm 
 sudo apt install terminator 
-sudo apt install code 
